9b5fd3d9b58a7b42121442753a3e1f48665f53bf
En-regalia <mike.ranner1@gmail.com>
2024-04-12 10:15:05 +0100

    Added function to pull top 10 into an array iterating over i

--- qa_wolf_take_home/qa_wolf_take_home/index.js
+++ qa_wolf_take_home/qa_wolf_take_home/index.js
@@ -1,5 +1,6 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
+const fs = require("FS");
 
 async function saveHackerNewsArticles() {
   // launch browser
@@ -9,6 +10,10 @@ async function saveHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com");
+
+  const topArticles = await page.$$eval(".itemlist tbody tr", (rows) => {
+    const articles = []
+    for (let i = 0; i < 10; i++) {
 }
 
 (async () => {
