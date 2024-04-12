ec61bc2f3287eeade98b4e111b9e3c80d1e0137b
En-regalia <mike.ranner1@gmail.com>
2024-04-12 14:16:09 +0100

    Added save to CSV and confimration message to the index

--- qa_wolf_take_home/qa_wolf_take_home/index.js
+++ qa_wolf_take_home/qa_wolf_take_home/index.js
@@ -1,6 +1,6 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
-const fs = require("FS");
+const fs = require("fs");
 
 async function saveHackerNewsArticles() {
   // launch browser
@@ -11,9 +11,33 @@ async function saveHackerNewsArticles() {
   // go to Hacker News
   await page.goto("https://news.ycombinator.com");
 
-  const topArticles = await page.$$eval(".itemlist tbody tr", (rows) => {
-    const articles = []
+  await page.waitForSelector("#hnmain");
+  
+  // Const to call top 10 articles from page defined in row 12
+  const topArticles = await page.$$eval('.titleline', (rows) => {
+    // defined array which will be returned at end of function
+    const articles = [];
+    // for loop to evaluate the page and extract URL and titles of articles 1-10
     for (let i = 0; i < 10; i++) {
+      const row = rows[i];
+      const titleElement = row.querySelector(".titleline a");
+      const url = titleElement.href;
+      const title = titleElement.innerText.trim();
+      // push extracted title and url to array
+      articles.push({title, url});
+    }
+    return articles;  
+  });
+
+  // save data to CSV file
+  const csvData = topArticles.map(article => `${article.title}, ${article.url}`).join("\n");
+  fs.writeFileSync("Hacker_news_Top_10_TEST.csv", csvData);
+
+  //Add message confirming outcome
+  console.log("Top 10 articles have been saved to Hacker_news_Top_10_TEST.csv");
+
+  //closing Browser
+  await browser.close();
 }
 
 (async () => {
